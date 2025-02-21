66f5769c365f34bda30e93da351c29c16770352a
Arpit Raj <130226536+arpit-raj31@users.noreply.github.com>
2025-02-21 16:02:55 +0530

    Update User.LiveAccTransactions.js

--- Server/src/models/User.LiveAccTransactions.js
+++ Server/src/models/User.LiveAccTransactions.js
@@ -1,7 +1,4 @@
-
-
 import mongoose from 'mongoose';
-
 const transactionLiveSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: { type: String, enum: ['deposit', 'withdrawal', 'trade'], required: true },
@@ -11,11 +8,7 @@ const transactionLiveSchema = new mongoose.Schema({
     unique: true,
   },
   amount: { type: Number, required: true },
-<<<<<<< HEAD
-  status: { type: String, enum: ['success', 'failure', 'pending'], default: 'pending' },
-=======
   status: { type: String, enum: ['success', 'failed', 'pending'], default: 'pending' },
->>>>>>> 1f35004 (New Changes)
   description: { type: String, trim: true },
 }, { timestamps: true });
 
